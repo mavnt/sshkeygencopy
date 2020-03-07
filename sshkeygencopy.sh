764e556a205c096e3b7aaf8c426f3847d8c06085
Mirko Avantaggiato <mirko.avantaggiato@gmail.com>
2020-03-07 16:27:32 +0100

    formatting

--- sshkeygencopy.sh
+++ sshkeygencopy.sh
@@ -8,13 +8,14 @@ else
     user=$1
     host=$2
     port=${3:-"22"}
-    uah=$user@$host  # user at host
+    uah=$user@$host # user at host
     echo "creating key pair for user $uah"
     ssh-keygen -t rsa -b 4096 -f "$uah" -N ""
     echo "copying pub key for user $user to host $host"
     ssh-copy-id -p $port -o PubkeyAuthentication=no -f -i $uah.pub $uah
     cp $uah* ~/.ssh
     rm $uah*
+
     echo "testing keys"
     ssh -i ~/.ssh/$uah -p $port -t $uah "echo hello from remote host"
     if [ $? -ne 0 ]; then
